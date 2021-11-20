58e08d6f78ef5efdd32512abd238a43e87f09f27
Manav-arora-87 <aroramanav887@gmail.com>
2021-11-20 12:18:03 +0530

    commit

--- adminweb/views.py
+++ adminweb/views.py
@@ -16,7 +16,11 @@ def Login(request):
 
 
 def CheckAdminLogin(request):
-
+    try:
+        result = request.session['ADMIN']
+        return render(request, "admin/Dashboard.html", {'result': result})
+    except  Exception as e:
+        pass
     try:
         emailid = request.POST['emailid']
         password = request.POST['password']
@@ -24,6 +28,8 @@ def CheckAdminLogin(request):
         # Adminlogins.ob
         if bcrypt.checkpw(password.encode("utf8"), admin.password.encode("utf8")):
             result=Surveyinfo.objects.filter(~Q(order_id__exact="")|~Q(voucher__exact="")).count()
+            if (result):
+                request.session['ADMIN'] = result
             print((result))
             return render(request, "admin/Dashboard.html",{'result':result})
         else:
@@ -44,12 +50,8 @@ def Info(request):
         pendingempverified = Surveyinfo.objects.filter(~Q(order_id__exact="")|~Q(voucher__exact="")).filter(~Q(profile_img_status=1) & ~Q(aadhar_front_uri_status=1) & ~Q(aadhar_back_uri_status=1) & ~Q(driving_licence_status=1) & ~Q(full_size_img_status=1) & ~Q(college_id_img_status=1) & ~Q(tenth_img_status=1) & ~Q(twelth_img_status=1) & ~Q(address_img_status=1) & ~Q(vaccination_img_status=1) & ~Q(police_verification_img_status=1) & ~Q(semester_img_status=1)).count()
         totalemponboard = Surveyinfo.objects.filter(~Q(order_id__exact='') | ~Q(voucher__exact='')).filter(survey_id__in=SurveyLogin.objects.filter(work_status=1)).count()
         pendingemponboard = Surveyinfo.objects.filter(~Q(order_id__exact='') | ~Q(voucher__exact='')).filter(survey_id__in=SurveyLogin.objects.filter(work_status=0)).count()
-        print(totalemponboard)
-        print(pendingemponboard)
-
 
         return render(request,'admin/Info.html',{'pendingemponboard':pendingemponboard,'totalemponboard':totalemponboard,'pendingempverified':pendingempverified,'totalempverified':totalempverified,'todayregistered':todayregistered,'totalregisterd':totalregistered})
-
     except Exception as e:
         print("Error",e)
-        return render(request,'admin/Info.html',{'todayregistered':0,'totalregisterd':9})
+        return render(request,'admin/Info.html')
